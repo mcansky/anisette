9a7b6da53f50fde092e11793b18a8506391da7a8
thom <riboulet@gmail.com>
2009-06-30 20:05:32 +0200

    creating the base db schema

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,7 +6,7 @@
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
   :key         => '_anisette_session',
-  :secret      => '8d86ef9ee0787f911cdcc2a5f10460dd88e50be8fd1f4d61654b31dd1ec887c9f457d461ca1d5bfe0461da1589749001e323a1565afdd06cfb0ba963babc6d48'
+  :secret      => '5f2631435eaa8b929480d3fc2f49e8c8d268b8f347753c98aba5177c5d264bbab52e97b17b999288de7c36438a4dde51c48447c29bd9c7d119d081ba5bba1c33'
 }
 
 # Use the database for sessions instead of the cookie-based default,
