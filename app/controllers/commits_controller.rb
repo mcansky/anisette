836a2dd65fc2d1954ef8c901f5ea5713ab1e376e
thom <riboulet@gmail.com>
2009-07-01 17:05:07 +0200

    changing commit controller to commits controller

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -1,8 +1,8 @@
-class CommitController < ApplicationController
+class CommitsController < ApplicationController
 
   def get
     @commit = AnisetteCommit.find(params[:id])
     @project = @commit.branch.repository.project
+    render :layout => 'project'
   end
-
 end
