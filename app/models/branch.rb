a680ee2fd958bf3b6ec752fcc8fbce5188e3daaf
thom <riboulet@gmail.com>
2009-07-02 01:15:10 +0200

    improving display on the top index page, adding some minor info display

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,4 +1,5 @@
 class Branch < ActiveRecord::Base
   belongs_to :repository
   has_many :commits, :class_name => "AnisetteCommit", :order => "commited_time"
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,17 @@
 class Project < ActiveRecord::Base
   has_many :repositories
   belongs_to :owner, :class_name => "User", :foreign_key => "owner_id"
+
+  def stats
+    c_count = 0;
+    b_count = 0;
+    r_count = 0;
+    r_count = self.repositories.count
+    self.repositories.each do |r|
+      b_count += r.branches.count
+      c_count += r.commits.count
+    end
+    stats = "#{c_count} commits in #{b_count} branches in #{r_count} repositories"
+  end
+
 end
