bed3dae8bb6e0515bf55a717aa53dcbe66656918
thom <riboulet@gmail.com>
2010-01-30 14:22:59 +0100

    adding gem cutter source

--- config/environment.rb
+++ config/environment.rb
@@ -22,7 +22,7 @@ Rails::Initializer.run do |config|
 	config.gem "coderay"
 	config.gem "grit"
 	config.gem "authlogic"
-	config.gem "will_paginate"
+	config.gem "will_paginate", :source => "http://gemcutter.org"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
